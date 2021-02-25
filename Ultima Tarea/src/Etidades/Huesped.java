2be8a5819cfc2ae0602fcf1f59ecd51465492b8a
Diego Huamanjulca <68879310+Alexair21@users.noreply.github.com>
2021-02-25 09:58:01 -0500

    Agregue lo que tenia

---
+++ Ultima Tarea/src/Etidades/Huesped.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Etidades;
+
+/**
+ *
+ * @author DIEGO HUAMANJULCA G
+ */
+public class Huesped {
+    private String nombre_apellido;
+    private int telefono;
+    private String nacionalidad;
+    private int n_documento;
+
+    public Huesped(String nombre_apellido, int telefono, String nacionalidad, int n_documento) {
+        this.nombre_apellido = nombre_apellido;
+        this.telefono = telefono;
+        this.nacionalidad = nacionalidad;
+        this.n_documento = n_documento;
+    }
+
+    public String getNombre_apellido() {
+        return nombre_apellido;
+    }
+
+    public void setNombre_apellido(String nombre_apellido) {
+        this.nombre_apellido = nombre_apellido;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public int getN_documento() {
+        return n_documento;
+    }
+
+    public void setN_documento(int n_documento) {
+        this.n_documento = n_documento;
+    }
+    
+    
+}
